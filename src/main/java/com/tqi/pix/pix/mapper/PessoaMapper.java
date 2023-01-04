3423a1aef5878b7fb40cf2f6ba71cd187e7a1bce
lanyablue <barbaramy_@hotmail.com>
2023-01-04 17:53:29 -0300

    [v0.7] - feat: pessoa mapper

---
+++ src/main/java/com/tqi/pix/pix/mapper/PessoaMapper.java
@@ -0,0 +1,34 @@
+package com.tqi.pix.pix.mapper;
+
+import com.tqi.pix.pix.model.dto.PessoaDTO;
+import com.tqi.pix.pix.model.Pessoa;
+import com.tqi.pix.pix.model.form.PessoaForm;
+import lombok.NoArgsConstructor;
+
+
+public final class PessoaMapper {
+
+    private PessoaMapper(){
+
+    }
+
+    // Para transformar o form em entidade, para receber o formulario
+    public static Pessoa formParaEntidade(PessoaForm pessoaForm) {
+        return Pessoa.builder()
+                .id(pessoaForm.getId())
+                .nome(pessoaForm.getNome())
+                .cpf(pessoaForm.getCpf())
+                .email(pessoaForm.getEmail())
+                .build();
+
+    }
+
+    // Para transformar a entidade em dto
+    public static PessoaDTO entidadeParaDto(Pessoa pessoa){
+        return PessoaDTO.builder()
+                .id(pessoa.getId())
+                .nome(pessoa.getNome())
+                .build();
+    }
+
+}
